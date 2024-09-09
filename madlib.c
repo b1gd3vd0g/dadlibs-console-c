c85dfd18dd05e8feab77f3a5e7904cf571b90b8d
b1gd3vd0g <devin@bigdevdog.com>
2024-09-08 22:03:39 -0700

    Entrance point into the program.
    introduce game, prompt for template, call processDadLib, and print the story.

---
+++ madlib.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "process.h"
+#include "prompt.h"
+
+char *A_DAY_IN_THE_LIFE_OF_A_TEACHER =
+    "./templates/day_in_life_of_teacher.txt";
+
+int MAX_STORY_LEN = 5096;
+int MAX_FP_LEN = 128;
+
+int main(int argc, char **argv)
+{
+    printf("\nDad Libs\n");
+    printf("A C Program written by BigDevDog\n\n");
+    printf("Choose your template:\n\n");
+
+    printf("[1] A day in the life of a teacher\n\n");
+
+    int choice = promptInt(1);
+    char *templatefp;
+    if (choice == 1)
+    {
+        templatefp = A_DAY_IN_THE_LIFE_OF_A_TEACHER;
+    }
+
+    // allocate space for the story string.
+    char *story = (char *)calloc(MAX_STORY_LEN, sizeof(char));
+    // This is going to read the file, prompt the users for input, and write the
+    // constructed story to story.
+    processDadLib(templatefp, story);
+    // print the story to the user.
+    printf("%s\n", story);
+    // deallocate the story.
+    free(story);
+    return 0;
+}
